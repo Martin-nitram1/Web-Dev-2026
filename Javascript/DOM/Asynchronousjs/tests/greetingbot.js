1ecfd8440bff765740b6ecc0ecc5939e15da27e1
Martin.O <martintech.ke@gmail.com>
2026-03-27 22:42:39 +0300

    Tests completed

---
+++ Javascript/DOM/Asynchronousjs/tests/greetingbot.js
@@ -0,0 +1,25 @@
+let greeting = () => console.log("Hello! I'm your coding fun fact guide");
+
+greeting();
+
+let botName = "Pymate";
+
+let botLocation= "Nairobi";
+
+let favoriteLanguage= "Python";
+
+console.log(`My name is ${botName}and i live on ${botLocation}.`);
+
+console.log(`my favorite language is ${favoriteLanguage}`);
+
+let codingFact = favoriteLanguage + " is a very dynamic Language";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was the first language i learnt";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " seems hard to alot of people but isnt that hard";
+console.log(codingFact)
+
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
---
+++ Javascript/DOM/Asynchronousjs/tests/sentenceMaker.js
@@ -0,0 +1,22 @@
+let adjective = "Smart";
+let noun = "guy";
+let verb = "breathing ";
+let place = "Mansion ";
+let adjective2 = "impressive";
+let noun2 = "Pizza";
+
+let firstStory = "Once upon a time, there was a(n)" + adjective + noun + "who loved to eat" + 
+ noun2 + ". The" + noun + "lived in a" + place + "and had" +  adjective2 + "nostrils that blew fire when it was" + verb + ".";
+
+console.log(firstStory);
+
+adjective = " insane"
+noun = "maniac"
+verb = "gossiping"
+place = "new mansion"
+adjective2 = "psycho"
+noun2 = "ingrate"
+
+let secondStory = "some weren't sure how much everyone else thought of this" + adjective + noun + "who seemed to enjoy" +  noun2 +". The" + noun + "moves to a" + place + "and had" +  adjective2 + "his mouth now blew fire when it was" + verb + ".";
+
+console.log(secondStory);
