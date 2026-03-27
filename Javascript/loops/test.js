2bb216862ba55114a3df2e1edb060ff4fe0f811e
Martin.O <martintech.ke@gmail.com>
2026-03-27 12:51:14 +0300

    track files

---
+++ Javascript/loops/test.js
@@ -0,0 +1,17 @@
+
+    let name = "Martin";
+    let currentAge = 30;
+    let enjoyCoding = true;
+
+    let newAge = (currentAge + 1);
+    let remainder = (newAge % 5); 
+    let age = String(newAge);
+    let canVote = (currentAge >= 18);
+
+    console.log(`name: ${name}`);
+    console.log(`currentAge: ${currentAge}`);
+    console.log(`enjoyCoding: ${enjoyCoding}`);
+    console.log(`newAge: ${newAge}`);
+    console.log(`remainder: ${remainder}`);
+    console.log(`age: ${age}`);
+    console.log(`canVote: ${canVote}`);
